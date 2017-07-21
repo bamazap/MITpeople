e5c335ee98a4a3e260d7c1bf5906fa3ac6073f25
Barry Anthony McNamara III <bamazap@hotmail.com>
2017-07-20 23:12:42 -0400

    MIT People Directory web scraper

---
+++ MITpeople.py
@@ -0,0 +1,70 @@
+import requests
+try: 
+    import bs4 as BeautifulSoup #python3
+    version = 3
+except ImportError:
+    import BeautifulSoup #python2
+    version = 2
+
+# options for options:
+# general -- name/email
+# phone -- reverse lookup (10 digits)
+# lastnamesx -- lastname sounds like
+def get_people(query, options="general", recurse=False):
+    """Accesses the MIT People Directory
+
+    Args:
+        query (str): The name, email, or phone number to search for
+        options (str, optional): The type of search to do. Should be one of
+            "general" -- name/email
+            "phone" -- reverse lookup (10 digits)
+            "lastnamesx" -- lastname sounds like
+        recurse (bool, optional): Whether or not additional requests should
+            be made to retrieve all fields when multiple results are found.
+            Defaults to False.
+    
+    Returns:
+        list: The people found.
+            Each element is a dictionary containing the person's details.
+            The dictionary will at least contain a "name" field.
+
+    """
+
+    url = "http://web.mit.edu/bin/cgicso?options="+options+"&query="+query
+    response = requests.get(url)
+    html = response.content
+
+    if version == 2:
+        soup = BeautifulSoup.BeautifulSoup(html)
+    if version == 3:
+        soup = BeautifulSoup.BeautifulSoup(html, "html.parser")
+    payload = soup.find("pre")
+
+    output = []
+
+    # case 1: no matches
+    if "No matches to your query." in payload.text:
+        return output
+
+    # case 2: multiple matches
+    if isinstance(payload.contents[0], BeautifulSoup.Tag):
+        if payload.contents[0].name == "a":
+            for a in payload.findAll("a", href=True):
+                if recurse:
+                    raise NotImplementedError #TODO
+                else:
+                    output.append({"name": a.contents})
+            return output
+
+    # case 3: one match
+    for content in payload.contents:
+        user_dict = {}
+        if isinstance(content, BeautifulSoup.Tag):
+            user_dict["email"] = content.contents[0]
+        else:
+            for field_val in map(lambda s: s.strip(), content.split("\n")):
+                if not (field_val == "" or field_val.startswith("email:")):
+                    field, val = field_val.split(' ', 1)
+                    user_dict[field] = val.strip()
+        output.append(user_dict)
+    return output
---
+++ README.md
@@ -0,0 +1,1 @@
+MIT People Directory web scraper
