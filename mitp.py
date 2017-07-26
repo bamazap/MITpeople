8514475f8e5bcf6c7cfeae54b3e563430c96b456
Barry Anthony McNamara III <bamazap@hotmail.com>
2017-07-26 13:45:18 -0400

    Script pretty-prints results.

--- mitp.py
+++ mitp.py
@@ -3,12 +3,26 @@ import sys
 from MITpeople import get_people
 
 parser = argparse.ArgumentParser(description="Access the MIT People Directory")
+
 parser.add_argument("query", type=str,
     help="The name, kerberos, number, or last name pronunciation to query.")
-parser.add_argument("-o", "--options", help="The type of query to run.",
-        default="general")
-parser.add_argument("-r", "--recurse", help="Follow links to get all data.",
-    action="store_true")
+
+options = parser.add_mutually_exclusive_group()
+options.add_argument("-g", "--general", action="store_const", dest="options",
+    const="general", help="Search by name or kerberos.")
+options.add_argument("-p", "--phone", action="store_const", dest="options",
+    const="phone", help="Search by ten-digit phone number.")
+options.add_argument("-l", "--lastnamesx", action="store_const", dest="options",
+    const="lastnamesx", help="Search by last name sounds like.")
+parser.set_defaults(options="general")
+
+parser.add_argument("-r", "--recurse", action="store_true",
+        help="Follow links to get all data.")
                                       
 args = parser.parse_args()
-print(get_people(args.query, args.options , args.recurse)) # works in v2 or v3
+
+people = get_people(args.query, args.options, args.recurse)
+for person in people:
+    for key in person:
+        print(key+": "+person[key]) # works in python 2 or 3:
+    print("") # space between results
