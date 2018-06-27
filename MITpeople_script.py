0f6f050ffa60423e69e7904cb7bfb182a1066eeb
Barry McNamara <bamazap@hotmail.com>
2018-06-27 10:25:46 -0400

    Fields show in a predictable order.

--- MITpeople_script.py
+++ MITpeople_script.py
@@ -17,11 +17,28 @@ parser.set_defaults(options="general")
 
 parser.add_argument("-r", "--recurse", action="store_true",
         help="Follow links to get all data.")
-                                      
+
 args = parser.parse_args()
+
+key_indexes = {
+    "name": 0,
+    "email": 1,
+    "phone": 2,
+    "address": 3,
+    "department": 4,
+    "title": 5,
+    "url": 6,
+    "school": 7,
+    "year": 8,
+}
+
+def get_key_idx(key):
+    return key_indexes.get(key, 9)
 
 people = get_people(args.query, args.options, args.recurse)
 for person in people:
-    for key in person:
+    keys = person.keys()
+    keys.sort(key=get_key_idx)
+    for key in keys:
         print(key+": "+person[key]) # works in python 2 or 3:
     print("") # space between results
